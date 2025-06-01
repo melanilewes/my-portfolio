b18341c0aeca82c41fb4d26b3155bd9cc2c787f6
VargasChristopher <melanilewes@outlook.com>
2025-06-01 17:28:20 -0400

    config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ].filter(Boolean),
+  base: "/my-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
